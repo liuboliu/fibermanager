a2fca58162202ce759b1ac047955d5fb02720725
liuboliu <2534660131@qq.com>
2018-03-12 11:11:31 +0800

    添加文件

---
+++ FiberstoreManager/Myview.m
@@ -0,0 +1,22 @@
+//
+//  Myview.m
+//  FiberstoreManager
+//
+//  Created by apple on 2018/3/12.
+//  Copyright © 2018年 123. All rights reserved.
+//
+
+
+#import "Myview.h"
+
+@implementation Myview
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
