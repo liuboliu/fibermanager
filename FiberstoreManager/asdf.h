078c8eb44fd6a5a68b93adc2ca35c5826b9221d3
liuboliu <2534660131@qq.com>
2019-10-13 11:10:15 +0800

    tijiao

---
+++ FiberstoreManager/asdf.h
@@ -0,0 +1,17 @@
+//
+//  asdf.h
+//  FiberstoreManager
+//
+//  Created by Apple on 2019/10/13.
+//  Copyright © 2019 123. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface asdf : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
